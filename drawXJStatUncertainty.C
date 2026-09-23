c980876df2f708f8d78008e5b7b87e61368a5b55
Virginia Bailey <vbailey2@illinois.edu>
2026-09-23 13:45:48 -0400

    add atlas comparison

--- drawXJStatUncertainty.C
+++ drawXJStatUncertainty.C
@@ -13,6 +13,13 @@ void drawXJStatUncertainty()
 	std::string cent_str[] = {"0-20%", "20-40%", "40-60%", "60-80%"};
 	int colors[] = {1, 2, 4, kGreen + 2, kViolet};
 
+	// ATLAS O+O/pp reference points (extracted from plots/oodijetatlas.pdf), overlaid on the inclusive plots
+	TGraph *gAtlas = new TGraph("atlas_oodijet_ratio.txt");
+	gAtlas->SetName("gAtlas");
+	gAtlas->SetMarkerStyle(24);
+	gAtlas->SetMarkerColor(kBlack);
+	gAtlas->SetLineColor(kBlack);
+
 	// per-centrality final xj histograms, plus their sum for the centrality-inclusive O+O comparison
 	TH2F *h_xj[ncent];
 	TH2F *h_xj_inclusive = nullptr;
@@ -31,15 +38,11 @@ void drawXJStatUncertainty()
 	{
 		TGraphAsymmErrors *gpp = (TGraphAsymmErrors *)fpp->Get(Form("g_final_xj_statistics_%d_1", ipt));
 
-		TCanvas *c = new TCanvas(Form("c_xjstat_pt%i", ipt), Form("c_xjstat_pt%i", ipt), 700, 700);
-		TLegend *leg = new TLegend(.65, .65, .88, .88);
-		leg->SetFillStyle(0);
-
-		bool drawnany = false;
 		for (int i = 0; i <= ncent; i++) // i = 0..ncent-1: centralities; i = ncent: centrality-inclusive
 		{
-			TH2F *hsrc = (i < ncent) ? h_xj[i] : h_xj_inclusive;
-			std::string label = (i < ncent) ? cent_str[i] : "Inclusive";
+			bool isInclusive = (i == ncent);
+			TH2F *hsrc = isInclusive ? h_xj_inclusive : h_xj[i];
+			std::string label = isInclusive ? "Inclusive" : cent_str[i];
 
 			hsrc->GetYaxis()->SetRange(ipt + 1, ipt + 1);
 			TH1D *h1d = (TH1D *)hsrc->ProjectionX(Form("h_xj_proj_%d_pt%d", i, ipt));
@@ -85,18 +88,25 @@ void drawXJStatUncertainty()
 			gratio->GetXaxis()->SetTitle("x_{J}");
 			gratio->GetYaxis()->SetTitle("stat. uncertainty on AA/pp");
 
-			if (!drawnany)
-			{
-				gratio->Draw("AP");
-				drawnany = true;
-			}
-			else
-				gratio->Draw("P SAME");
-			leg->AddEntry(gratio, label.c_str(), "lep");
-		}
+			TCanvas *c = new TCanvas(Form("c_xjstat_pt%d_%d", ipt, i), Form("c_xjstat_pt%d_%d", ipt, i), 700, 700);
+			TLegend *leg = new TLegend(.65, .72, .88, .88);
+			leg->SetFillStyle(0);
 
-		leg->Draw();
-		c->Print(Form("plots/xj_statuncertainty_pt%i.pdf", ipt));
+			gratio->Draw("AP");
+			leg->AddEntry(gratio, label.c_str(), "lep");
+
+			std::string outname = isInclusive ? Form("plots/xj_statuncertainty_pt%i_inclusive.pdf", ipt)
+											   : Form("plots/xj_statuncertainty_pt%i_cent%i.pdf", ipt, i);
+
+			if (isInclusive)
+			{
+				gAtlas->Draw("P SAME");
+				leg->AddEntry(gAtlas, "ATLAS O+O/pp (0-10%)", "lep");
+			}
+
+			leg->Draw();
+			c->Print(outname.c_str());
+		}
 	}
 
 	std::cout << "all done" << std::endl;
